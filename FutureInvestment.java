6db579d744a2054e832e83d1c4c01828a11c15ef
shivkanyafuke26 <115626183+shivkanyafuke26@users.noreply.github.com>
2024-03-14 09:43:33 +0530

    Update FutureInvestment.java

--- FutureInvestment.java
+++ FutureInvestment.java
@@ -25,28 +25,4 @@ System.out.print("Enter the side length of the hexagon: "+futureInvestmentValue)
 }
 
 }       
-      /*
-
-
-import java.util.Scanner;
-
-class FutureInvestment  {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the balance: ");
-        double balance = sc.nextDouble();
-        System.out.print("Enter the annual interest rate (in percentage): ");
-        double annualInterestRate = sc.nextDouble();
-
-        double monthlyInterestRate = annualInterestRate / 12 / 100;
-
-        double interestForNextMonth = balance * monthlyInterestRate;
-
-        System.out.println("Interest for the next month: " + interestForNextMonth);
-
-        }
-}
-
-
-*/
+      
