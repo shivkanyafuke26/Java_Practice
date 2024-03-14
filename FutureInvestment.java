18ee6d85a9910b78c24eaf52a5366ce58531c1ef
shivkanyafuke26 <115626183+shivkanyafuke26@users.noreply.github.com>
2024-03-14 09:43:21 +0530

    Update FutureInvestment.java

--- FutureInvestment.java
+++ FutureInvestment.java
@@ -6,15 +6,15 @@ public static void main(String args[])
 {
 Scanner sc = new Scanner(System.in);
 
-System.out.print("Enter the side length of the hexagon: ");
+System.out.print("Enter the investment Amount : ");
 
 double investmentAmount = sc.nextDouble();
  
-System.out.print("Enter the side length of the hexagon: ");
+System.out.print("Enter the monthly interestRate: ");
 
 double monthlyinterestRate = sc.nextDouble();
         
-System.out.print("Enter the side length of the hexagon: ");
+System.out.print("Enter the number Of Years: ");
 
 double numberOfYears = sc.nextDouble();
                 
